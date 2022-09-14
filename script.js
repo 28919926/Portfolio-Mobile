b8430a655ba9ae8474ce7c5f0b3dc3cba443ff22
mansa susa <mansasusa18@gmail.com>
2022-09-14 06:03:21 -0400

    Add event listeners to the nav links

--- script.js
+++ script.js
@@ -14,3 +14,5 @@ function afterClick() {
   nav.classList.remove('active');
 }
 
+navLinks.forEach((n) => n.addEventListener('click', afterClick));
+nav.forEach((n) => n.addEventListener('click', showMenu));
