2131a14a7f7990e23610fd1f09c065717b8b3b04
Alex Ogeto <103739438+Osoro254Alex@users.noreply.github.com>
2022-09-14 12:56:00 +0300

    Added an Event listner to menu button

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ function hideMenu() {
   nav.style.right = '-500px';
   nav.classList.remove('active');
 }
+
+nav.forEach((n) => n.addEventListener('click', hideMenu));
+
+function afterClick() {
+  nav.classList.remove('active');
+}
+
