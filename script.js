81de9b7144762cdecf442472e53b6ca00e7cb135
28919926 <wanjaladesmond75@gmail.com>
2022-09-16 15:20:48 +0300

    add the changes on the .js file

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-// const nav = document.getElementById('rightNav');
-// const navLinks = document.getElementById('menu');
-// function showMenu() {
-//   nav.style.right = '0px';
-// }
-// function hideMenu() {
-//   nav.style.right = '-500px';
-//   nav.classList.remove('active');
-// }
-// nav.forEach((n) => n.addEventListener('click', hideMenu));
-=======
 const nav = document.getElementById('rightNav');
 const navLinks = document.getElementById('menu');
 function showMenu() {
@@ -21,7 +9,6 @@ function hideMenu() {
 }
 
 nav.forEach((n) => n.addEventListener('click', hideMenu));
->>>>>>> cdeaf684dba88a0289d57cc173b1fba3617287f7
 
 // function afterClick() {
 //   nav.classList.remove('active');
